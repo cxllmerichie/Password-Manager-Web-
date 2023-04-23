219e375e305734dacc68d538c9ae853a350bd244
Roman Lysychkin <cxllmerichie@gmail.com>
2023-04-23 12:52:31 +0200

    `ToDo` update

--- desktop/main.py
+++ desktop/main.py
@@ -13,10 +13,7 @@ if __name__ == '__main__':
 
 
 # ToDo: fix QPushButton.disabled Icon
-# ToDo: display micro-instruction below description during creation of category
-# ToDo: display "No items yet" in CentralPagesItems when category does not have items
 # ToDo: Fix 'elided' in CentralPagesItems when resizing SplitterWidgets
-# ToDo: Add search bar for items
 # ToDo: Display Category.title (or topic as All/Favourite) when displaying items
 # ToDo: when SplitterWidget resized to max resizing the app through Panel breaks
 # ToDo: Fix min/max App size (caused by min-size of inner widgets)
@@ -25,7 +22,6 @@ if __name__ == '__main__':
 # ToDo: user profile and settings
 # ToDo: local/remote storage (fetch from both if local chosen, any new added to local, colors of local/remote are different)
 
-# ToDo: create CACHE class for `qcontextapi` to store there categories, items, fields and ids instead of setting them as attributes
 # ToDo: add password generating procedure (fetch from api)
 # ToDo: create "Loader" using threads while waiting for displaying items in CentralPagesItems
 # ToDo: multilang (translations)
