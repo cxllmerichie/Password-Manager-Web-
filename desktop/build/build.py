eca1a25f32e1f4c4ff713f987a494351fbb91fbc
Roman Lysychkin <cxllmerichie@gmail.com>
2023-06-30 15:55:11 +0200

    pull

--- desktop/build/build.py
+++ desktop/build/build.py
@@ -4,7 +4,7 @@ from subprocess import PIPE, Popen
 name = 'PasswordManager'
 root = 'C:\\Projects\\Python\\PasswordManager\\desktop'
 main = f'{root}\\main.py'
-build = f'{root}\\build\\v8'
+build = f'{root}\\build\\v11'
 icon = f'{root}\\.assets\\icon.ico'
 command = f'pyinstaller -p {root} --onefile --windowed --specpath {build} --workpath {build} --distpath {build} --icon={icon} --name={name} {main}'
 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE)
